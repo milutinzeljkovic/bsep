b192a57128319531a6fe7990c7a62657b8f06ec4
milutin zeljkovic <milutin.zeljkovic@gmail.com>
2020-04-14 20:56:23 +0200

    bug fixed in AbstractBaseEntity

--- backend/src/main/java/de/jonashackt/springbootvuejs/model/AbstractBaseEntity.java
+++ backend/src/main/java/de/jonashackt/springbootvuejs/model/AbstractBaseEntity.java
@@ -15,6 +15,5 @@ abstract class AbstractBaseEntity {
     @Id
     @GeneratedValue(generator = "uuid2")
     @GenericGenerator(name = "uuid2", strategy = "uuid2")
-    @Type(type = "uuid-char")
     private UUID id;
 }
